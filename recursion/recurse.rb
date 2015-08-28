01747d77b3528705c6defe7eedd51302c7fd60aa
Crystal Hess <crystal@adadevelopersacademy.org>
2015-08-28 11:26:12 -0700

    adds recursion practice

---
+++ recursion/recurse.rb
@@ -0,0 +1,28 @@
+def fact(n)
+end
+
+def fib(n)
+end
+
+def pal(s)
+end
+
+# Factorial Tests
+raise "factorial broke - fact(4)" unless fact(4) == 24
+raise "factorial broke - fact(0)" unless fact(0) == 1
+puts "passes all factorial tests"
+
+# Fibanocci Tests
+raise "fib broke - fib(8)" unless fib(8) == 21
+raise "fib broke - fib(20)" unless fib(20) == 6765
+raise "fib broke - fib(1)" unless fib(1) == 1
+raise "fib broke - fib(2)" unless fib(2) == 1
+puts "passes all fibanocci tests"
+
+# Palindrome Tests
+raise "pal broke - pal('racecar')" unless pal("racecar") == true
+raise "pal broke - pal('helloworld')" unless pal("helloworld") == false
+raise "pal broke - pal('')" unless pal("") == true
+puts "passes all palindrome tests"
+
+puts "All test passed"

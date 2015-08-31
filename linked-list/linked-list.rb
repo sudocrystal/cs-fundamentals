528f7d595a44977fb67583e339b80749a65083d0
Crystal Hess <crystal@adadevelopersacademy.org>
2015-08-31 16:02:03 -0700

    class demo

--- linked-list/linked-list.rb
+++ linked-list/linked-list.rb
@@ -1,15 +1,37 @@
 require 'RubyDataStructures'
 
 def print_list(list)
-  puts "print_list not yet implemented\n\n"
+#  puts "print_list not yet implemented\n\n"
+  temp = list.head
+  puts temp.key
+  while temp.next != list.head
+    temp = temp.next
+    puts temp.key
+  end
 end
 
 def count_node(list)
-  return "count_node not yet implemented"
+#  return "count_node not yet implemented"
+  temp = list.head
+  count = 0
+  while temp.next != list.head
+    temp = temp.next
+    count += 1
+  end
+  return count
 end
 
 def find_min(list)
-  return "find_min not yet implemented"
+#  return "find_min not yet implemented"
+  temp = list.head
+  min = temp.key
+  while temp.next != list.head
+    if temp.key < min
+      min = temp.key
+    end
+    temp = temp.next
+  end
+  return min
 end
 
 # create a new linked list
@@ -19,6 +41,9 @@ my_list = RubyDataStructures::SinglyLinkedList.new
 my_list.insert(2)
 my_list.insert(0)
 my_list.insert(6)
+my_list.insert(-201)
+my_list.insert(29)
+my_list.insert(-111)
 
 # test methods above
 puts "What's in my list?"
--- tree/tree.rb
+++ tree/tree.rb
@@ -2,7 +2,10 @@ require 'tree'
 require 'tree/binarytree'
 
 def count_node(tree)
-  return "count_node not yet implemented"
+#  return "count_node not yet implemented"
+  return 0 if tree.nil?
+#  return 1 if tree.left_child.nil? && tree.right_child.nil?
+  return 1 + count_node(tree.left_child) + count_node(tree.right_child)
 end
 
 def find_min(tree, min = nil)
@@ -11,6 +14,9 @@ end
 
 # create a new tree, with a root of 1
 my_tree = Tree::BinaryTreeNode.new("1")
+my_tree.left_child = Tree::BinaryTreeNode.new("2")
+my_tree.left_child.right_child = Tree::BinaryTreeNode.new("4")
+my_tree.right_child = Tree::BinaryTreeNode.new("3")
 
 # put some stuff in the tree, namely:
 # * 100
@@ -24,7 +30,7 @@ my_tree = Tree::BinaryTreeNode.new("1")
 
 # test methods above
 puts "What's in my tree?"
-# my_tree.print_tree
+my_tree.print_tree
 
 puts "NODE COUNT = #{count_node(my_tree)}"
 puts "SMALLEST ELEMENT = #{find_min(my_tree)}"

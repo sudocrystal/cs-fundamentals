ff5e78159ee8eae11db95073693a564f7ab1ea1e
Annalee <annaleeherrera@gmail.com>
2015-11-15 23:57:59 -0800

    cs-fundamentals, stacks and queues

--- stack-queue/Queue.rb
+++ stack-queue/Queue.rb
@@ -2,43 +2,51 @@ class Queue
   def initialize(size)
     @size = size
     @store = Array.new(@size)
+    #the array is limited to the size
     @head, @tail = -1, 0
+    #the head is the last item, and the tail is the first.
   end
-  
+
   def dequeue
     if empty?
       nil
     else
       @tail = @tail.succ
-
+      #Incrementing a digit always results in another digit, and incrementing a letter results in another letter of the same case.
       dequeued = @store[@head]
       @store.unshift(nil)
+      #unshift will add a new item to the beginning of an array.
       @store.pop
+      #The method pop removes the last element in an array and returns it:
       dequeued
     end
   end
-  
+
   def enqueue(element)
     if full? or element.nil?
       nil
     else
       @tail = @tail.pred
+      #Returns the Integer equal to int - 1.  Will return the last item in queue.
       @store[@tail] = element
-      self 
+      #returns the first item
+      self
     end
   end
-  
+
   def size
     @size
   end
-  
+
   private
-  
+
   def empty?
     @head == -1 and @tail == 0
+    #will return true or false
   end
 
   def full?
     @tail.abs == (@size)
+    #will return true or false
   end
 end
--- stack-queue/Stack.rb
+++ stack-queue/Stack.rb
@@ -2,45 +2,54 @@ class Stack
   def initialize(size)
     @size = size
     @store = Array.new(@size)
+    #array size is limited
     @top = -1
+    #the top will always be the last item in a stack
   end
-  
+
   def pop
+    #Removes the last element from self and returns it, or nil if the array is empty.
     if empty?
       nil
     else
       popped = @store[@top]
+      #Will pop off last item in the array
       @store[@top] = nil
       @top = @top.pred #@top--
+      #Returns the Integer equal to int - 1.
       popped
     end
   end
-  
+
   def push(element)
     if full? or element.nil?
       nil
     else
       @top = @top.succ
+      # Incrementing a digit always results in another digit, and incrementing a letter results in another letter of the same case.
       @store[@top] = element
       self
     end
   end
-  
+
   def size
     @size
   end
-  
+
   def look
     @store[@top]
+    #@top = -1, will give the last item in @store
   end
-  
+
   private
-  
+
   def full?
     @top == (@size - 1)
+    #will return true or false for full
   end
-  
+
   def empty?
     @top == -1
+    #will return true or false for empty
   end
 end

c2a2016801f4a7ddea31302fc75c80c4deef1789
Meighan Rasley <meighanr@gmail.com>
2015-12-05 17:12:42 -0800

    Code and comments for insertion and selection sort

--- sorting/sorts.rb
+++ sorting/sorts.rb
@@ -8,18 +8,68 @@ def create_array
 	return array
 end
 
+
+
 # TODO: write insertion sort
-def insertion_sort(arr)
-  # starts at 2nd # and checks to see if it's less than the first #, if it is, it swaps them
-  
-	return arr
+# starts at 2nd # and checks to see if it's less than the first #, if it is, it swaps them
+def insertion_sort(array)
+  # setting an array called final to [whatever is at the first position of the array]; an array of length 1
+  final = [array[0]]
+  # deleting the first index of the array so that it doesn't get considered in the while loop, since it is already in final array
+  array.delete_at(0)
+  # iterating through the array
+  array.each do |i|
+    # setting a value of 0 to a variable called final_index
+    final_index = 0
+    # while final_index value is less than the length of final array
+    while final_index < final.length
+        # if i is less than or equal to final[0]
+        if i <= final[final_index]
+            # then insert i into final array at position indicated by final_index value (i.e., i swaps positions with the value at the index indicated by final_index)
+            final.insert(final_index, i)
+            # breaks the loop when if returns true
+            break
+        # else if final_index equals the length of final - 1
+        elsif final_index == final.length - 1
+            # then insert i into final array at position indicated by final_index value plus 1 (i.e, i gets inserted behind the value at final_index plus 1)
+            final.insert(final_index + 1, i)
+            # breaks the loop when elsif returns true
+            break
+        end
+        # add 1 to final_index each time it runs through the loop
+        final_index += 1
+    end
+  end
+  # puts final array
+  final
 end
 
 # TODO: write selection sort
-def selection_sort(arr)
   # starts at 1st number and assumes it's the smallest, then goes to the second # and sees if it's smaller than the first,
   # if it is, it swaps them
-	return arr
+def selection_sort(array)
+  # creates a copy of array and assigns it to a variable called sort
+  sort = array.dup
+  # creates an empty array called sorted
+  sorted = []
+  # until the length of sorted array equals the length of the passed in array
+  until sorted.length == array.length
+    # set min equal to whatever value is at sort index 0
+    min = sort[0]
+    # iterate through sort array for as many elements sort array has
+    sort.length.times do |i|
+      # reset min to equal the value of sort at position i only if that value is less than the value at index 0
+      min = sort[i] if sort[i] < min
+      end
+      # push min to the sorted array
+      sorted.push(min)
+      # deletes the element in sort array where the index equals min
+      sort.delete_at(sort.index(min))
+  end
+    # sets array equal to sorted
+    array = sorted
+    # returns the sorted array
+    return array
 end
 
 begin
@@ -50,3 +100,52 @@ puts "\n\n"
 
 
 puts "YAY!! I wrote Insertion sort and Selection sort in Ruby!! Aren't I awesome?"
+
+# def swap(array, current_i, next_i)
+#   temp = array[next_i]
+#   array[next_i] = array[current_i]
+#   array[current_i] = temp
+# end
+#
+# def smaller?(array, current_i, next_i)
+#   if array[next_i] < array[current_i]
+#     return true
+#   end
+# end
+#
+# def start?(next_i)
+#   if next_i == 0
+#     return true
+#   end
+# end
+#
+# def finish?(array, next_i)
+#   if next_i = array.length
+#     return true
+#   end
+# end
+#
+# def set_index(current_i, next_i, index)
+#   current_i = index
+#   next_i = index + 1
+# end
+#
+# def check_left(array, current_i, next_i, index)
+#   while !start?(next_i)
+#     if smaller?(array, current_i, next_i)
+#       swap(array, current_i, next_i)
+#       current_i -= 1
+#       next_i -= 1
+#     else
+#       set_index(current_i, next_i, index)
+#       break
+#     end
+#   end
+#   set_index(current_i, next_i, index)
+# end
+#
+# def step_forward(current_i, next_i, index)
+#   current_i += 1
+#   next_i += 1
+#   index += 1
+# end

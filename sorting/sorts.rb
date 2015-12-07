4f37a063d66cf526094b169f58611905a8875cc7
Daphne <dg1181@nyu.edu>
2015-12-07 08:51:29 -0800

    sorts thing

--- sorting/sorts.rb
+++ sorting/sorts.rb
@@ -9,13 +9,44 @@ def create_array
 	return array
 end
 
-# TODO: write insertion sort 
+# insertion sort examines each element (in order of index from 0 to n-1 where n is the length of the array), then inserts that element at its appropriate place within the group of sorted elements
+# performing a series of element swaps adjusts for the shifting indices during an "insertion"
+# TODO: write insertion sort (done)
 def insertion_sort(arr)
+
+	1.upto(arr.length - 1) do |n|
+		insert_me = arr[n]
+
+		until arr[n - 1] < insert_me || (n - 1) < 0
+			arr[n] = arr[n - 1]
+			arr[n - 1] = insert_me
+
+			n -= 1
+		end
+	end
+
 	return arr
 end
 
+# selection replaces each element in an array one-by-one (in order of index from 0 to n - 1 where n is the length of the array), by scanning the unsorted elements for the smallest value and performing a swap
 # TODO: write selection sort
 def selection_sort(arr)
+
+	arr.length.times do |swap_with_me|
+		selector = swap_with_me
+
+		swap_with_me.upto(arr.length - 1) do |i|
+			if arr[i] < arr[selector]
+				selector = i
+			end
+		end
+
+		smallest = arr[selector]
+
+		arr[selector] = arr[swap_with_me]
+		arr[swap_with_me] = smallest
+	end
+
 	return arr
 end
 

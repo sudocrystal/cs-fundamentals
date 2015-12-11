59b07ef5555275c72305f7784acc94892a64a107
Ricky <houglande@gmail.com>
2015-12-11 11:59:18 -0800

    Update recurse.rb

--- recursion/recurse.rb
+++ recursion/recurse.rb
@@ -1,10 +1,32 @@
 def fact(n)
+  if n == 1 || n == 0
+    return 1
+  end
+  return n * fact(n-1)
 end
 
 def fib(n)
+  if n == 1 || n == 2
+    return 1
+  end
+  return fib(n - 1) + fib(n - 2)
 end
 
 def pal(s)
+  if s.length == 1 || s.length == 0
+    return true
+  end
+
+  first = s[0]
+  last = s[-1]
+
+  if first == last
+    s = s.delete(first)
+    s = s.delete(last)
+    return pal(s)
+  else
+    return false
+  end
 end
 
 # Factorial Tests
@@ -20,6 +42,8 @@ raise "fib broke - fib(2)" unless fib(2) == 1
 puts "passes all fibanocci tests"
 
 # Palindrome Tests
+# base case, use length of the word
+# recursive case, throw out 2 letters of word each time you recurse
 raise "pal broke - pal('racecar')" unless pal("racecar") == true
 raise "pal broke - pal('helloworld')" unless pal("helloworld") == false
 raise "pal broke - pal('')" unless pal("") == true

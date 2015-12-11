7a8f22258ab03c8b6f8126f94c318b2a5d41cc20
Jennie Buechner <janina.buechner@gmail.com>
2015-12-11 11:58:03 -0800

    create recursive functions

--- recursion/recurse.rb
+++ recursion/recurse.rb
@@ -1,10 +1,26 @@
 def fact(n)
+  # base case
+  return 1 if n == 0 || n == 1
+  # recursive case
+  return n * fact(n - 1)
 end
 
 def fib(n)
+  # base case
+  return 1 if n == 1 || n == 2
+  # recursive case
+  return fib(n - 2) + fib(n - 1)
 end
 
 def pal(s)
+  # base case
+  return true if s.length == 0 || s.length == 1
+  if s[0] != s[-1]
+    return false
+  else
+    # recursive case
+    true and pal(s[1..-2])
+  end
 end
 
 # Factorial Tests
@@ -21,6 +37,7 @@ puts "passes all fibanocci tests"
 
 # Palindrome Tests
 raise "pal broke - pal('racecar')" unless pal("racecar") == true
+raise "pal broke - pal('abcddcba')" unless pal("abcddcba") == true
 raise "pal broke - pal('helloworld')" unless pal("helloworld") == false
 raise "pal broke - pal('')" unless pal("") == true
 puts "passes all palindrome tests"

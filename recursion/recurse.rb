848540c4de03c1a6b470a646192b114508cbe76c
Claire <ClaireS29@gmail.com>
2015-12-13 23:26:27 -0800

    Added recursive functions

--- recursion/recurse.rb
+++ recursion/recurse.rb
@@ -1,10 +1,32 @@
 def fact(n)
+  if n == 1 || n == 0 #base case
+    return 1
+  end
+  return n * fact(n-1) #recursive case
 end
 
 def fib(n)
+  if n < 1
+    return 0
+  elsif n == 2 || n == 1
+    return 1
+  end
+  return fib(n-1) + fib(n-2)
 end
 
 def pal(s)
+  if s.length <= 1
+    return true
+  end
+  w = s.split("")
+  l = w.pop
+  f = w.shift
+  s = w.join("")
+  if l == f
+    pal(s)
+  else
+    return false
+  end
 end
 
 # Factorial Tests
@@ -20,7 +42,7 @@ raise "fib broke - fib(2)" unless fib(2) == 1
 puts "passes all fibanocci tests"
 
 # Palindrome Tests
-raise "pal broke - pal('racecar')" unless pal("racecar") == true
+ raise "pal broke - pal('racecar')" unless pal("racecar") == true
 raise "pal broke - pal('helloworld')" unless pal("helloworld") == false
 raise "pal broke - pal('')" unless pal("") == true
 puts "passes all palindrome tests"

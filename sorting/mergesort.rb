4823b3d9221e750cf81b66befba9cf06ece01c89
Dreedle <davis.audrey.lynn@gmail.com>
2015-12-18 12:16:51 -0800

    Wrote mergesort

--- sorting/mergesort.rb
+++ sorting/mergesort.rb
@@ -1,31 +1,54 @@
+require 'pry'
+
 def mergesort(a)
-	# if the array size is 0|1 then the list is considered sorted, return sorted array
-	# split the list in half
-	# merge sort each half
-	# combine the sorted halves
+	if a.length <= 1
+		return a
+	end
+	#split list in half
+	left, right = split_array(a)
+	#merge sort each half
+	left = mergesort(left)
+	right = mergesort(right)
+	#combine sorted halves
+	return combine(left, right)
 end
 
 def split_array(a)
-	# find the middle
-  	# take = Returns first n elements from the array.
-	# drop = Drops first n elements and returns the rest of the elements.
-	# return left and right halves of array as separate arrays
-	# [a[0..mid] , a[mid+1..-1]]
-	# return a.take(mid), a.drop(mid)
+	half_size = ((a.length)/2).to_i #if array is 11 long, returns 5
+	left = a.take(half_size)
+	right = a.drop(half_size)
+	return left, right
 end
 
 # precondition: a and b are sorted
 def combine(a, b)
-	# create a results array
-	
-	# counters pointing to the index of the smallest elements in each array
 
-	# check that we have elements to compare
-		# push the smaller element onto the result array
+	result = []
 
-	# if there are elements left over in a, move them to result
-	# if there are elements left over in b, move them to result
-
+	#did this without iterating through indices, to see what would happen. I think it's uglier than it would be if I just iterated through indices.
+	while !!a[0] || !!b[0]
+		if !!a[0] && !!b[0]
+			#if both indices have something in them
+			#compare and move smaller to result
+			if a[0] < b[0]
+				min = a.shift(1)
+				result.push(min[0])
+			else #this accounts for the equal to or a is greater case. arbitrary which one we take when they are equal.
+				min = b.shift(1)
+				result.push(min[0])
+			end
+		else #either a or b is empty
+			#move whatever is leftover to result
+			if !!a[0]
+				min = a.shift(1)
+				result.push(min[0])
+			else
+				min = b.shift(1)
+				result.push(min[0])
+			end
+		end
+	end
+	return result
 end
 
 # TEST IT

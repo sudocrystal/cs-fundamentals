8ca9e4a848dc8df8cd1f1517cb8492152bb90f5b
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-19 11:53:48 -0800

    mergesort working;

--- sorting/mergesort.rb
+++ sorting/mergesort.rb
@@ -1,31 +1,47 @@
+require 'pry'
+
 def mergesort(a)
 	# if the array size is 0|1 then the list is considered sorted, return sorted array
-	# split the list in half
-	# merge sort each half
+  return a if a.length <= 1
+  # split the list in half
+  left, right =  split_array(a)
+  # merge sort each half
+  right = mergesort(right)
+  left = mergesort(left)
 	# combine the sorted halves
+  return combine(left, right)
 end
 
 def split_array(a)
-	# find the middle
-  	# take = Returns first n elements from the array.
-	# drop = Drops first n elements and returns the rest of the elements.
-	# return left and right halves of array as separate arrays
-	# [a[0..mid] , a[mid+1..-1]]
-	# return a.take(mid), a.drop(mid)
+  mid = (a.length / 2) - 1
+	left = a[0..mid]
+  right = a[mid+1..-1]
+	return left, right
 end
 
 # precondition: a and b are sorted
-def combine(a, b)
+def combine(left, right)
 	# create a results array
-	
-	# counters pointing to the index of the smallest elements in each array
-
+  results = []
 	# check that we have elements to compare
+  while right.length > 0 && left.length > 0
 		# push the smaller element onto the result array
-
-	# if there are elements left over in a, move them to result
-	# if there are elements left over in b, move them to result
-
+    if right[0] <= left[0]
+      results.push(right[0])
+      right.delete_at(0)
+    elsif left[0] < right[0]
+      results.push(left[0])
+      left.delete_at(0)
+    end
+  end
+  # if there are elements left over in a, move them to result
+  if !right.empty?
+    results.push(right)
+  # if there are elements left over in b, move them to result
+  elsif !left.empty?
+    results.push(left)
+  end
+  return results.flatten
 end
 
 # TEST IT

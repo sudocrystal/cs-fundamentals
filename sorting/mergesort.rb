7287813a281d84a168c102860989251057dc0e92
Meighan Rasley <meighanr@gmail.com>
2015-12-18 22:04:03 -0800

    Meighan MergeSort

--- sorting/mergesort.rb
+++ sorting/mergesort.rb
@@ -4,12 +4,12 @@ def mergesort(a)
 		return a
 	end
 	# split the list in half
-	left,right = split_array(a)
+	left, right = split_array(a)
 	# merge sort each half
-	mergesort(left)
-	mergesort(right)
+	left = mergesort(left)
+	right = mergesort(right)
 	# combine the sorted halves
-	return combine(left,right)
+	return combine(left, right)
 end
 
 def split_array(a)
@@ -18,9 +18,13 @@ def split_array(a)
 	# drop = Drops first n elements and returns the rest of the elements.
 	## each_slice is a ruby enumerator, round method rounds up
 	take,drop = a.each_slice( (a.size/2.0).round ).to_a
+	# mid = a.length / 2
+	# take = a[0..mid-1]
+	# drop = a[mid..-1]
 	# return left and right halves of array as separate arrays
-	return take,drop
+	##return left, right
 	# [a[0..mid] , a[mid+1..-1]]
+	return take, drop
 	# return a.take(mid), a.drop(mid)
 end
 
@@ -29,28 +33,22 @@ def combine(left, right)
 	# create a results array
 	results = []
 	# counters pointing to the index of the smallest elements in each array
-	first_counter = 0
-	second_counter = 0
+	# first_counter = 0
+	# second_counter = 0
 	# check that we have elements to compare
-if left.length > 0 && right.length > 0
-	if left[first_counter] >= right[second_counter]
-		# push the smaller element onto the result array
-		results.push(right[second_counter])
-		second_counter +=1
-	else
-		results.push(left[first_counter])
-		first_counter += 1
+	until left.length == 0 || right.length == 0
+		if left[0] <= right[0]
+			# push the smaller element onto the result array
+			results.push(left.slice!(0))
+		else
+			results.push(right.slice!(0))
+		end
 	end
-end
-	# if there are elements left over in a, move them to result
-	if left.length > 0
-		results.push(left)
-	end
-	# if there are elements left over in b, move them to result
-	if right.length > 0
-		results.push(right)
-	end
-
+		# if there are elements left over in a, move them to result
+			results += left if left.length != 0
+		# if there are elements left over in b, move them to result
+			results += right if right.length != 0
+	return results
 end
 
 # TEST IT

235b8d46d97845c403bb96f7abbc3642933520a4
Crystal Hess <crystal@adadevelopersacademy.org>
2016-01-08 10:01:28 -0800

    updates linked list assignment

--- linked-list/linked-list-practice.rb
+++ linked-list/linked-list-practice.rb
@@ -1,8 +1,4 @@
-require 'RubyDataStructures'
-
-def print_list(list)
-  puts "print_list not yet implemented\n\n"
-end
+require './List.rb'
 
 def count_node(list)
   return "count_node not yet implemented"
@@ -13,16 +9,16 @@ def find_min(list)
 end
 
 # create a new linked list
-my_list = RubyDataStructures::SinglyLinkedList.new
+my_list = List.new
 
 # put some stuff in the list
-my_list.insert(2)
-my_list.insert(0)
-my_list.insert(6)
+my_list.add(2)
+my_list.add(0)
+my_list.add(6)
 
 # test methods above
 puts "What's in my list?"
-print_list(my_list)
+puts my_list
 
 puts "NODE COUNT = #{count_node(my_list)}"
 puts "SMALLEST ELEMENT = #{find_min(my_list)}"

9a0edc2d26ffa960dcd3f9499baf555cb70c5561
Daphne <dg1181@nyu.edu>
2016-01-10 13:23:44 -0800

    Finished with List.rb

--- linked-list/List.rb
+++ linked-list/List.rb
@@ -13,21 +13,73 @@ class List
     # if the list is empty, add the first node
 	  if @head == nil
 	    @head = Node.new(data,nil)
-	  end
+	  else
     # if the list isn't empty, add after the last node
     # TODO: this case
+      temp = @head
+      while temp.next != nil
+        temp = temp.next
+      end
+      temp.next = Node.new(data,nil)
+      # THIS EXAMPLE ONLY
+      # b,a
+      # make b node
+      # new_node = Node.new(data, nil)
+      # make b point at the head
+      # new_node.next = @head
+      # reassign head
+      # @head = new_node
+      # @head = Node.new(data,@head)
+    end
   end
 
   def add_at_index(index, data)
     # TODO Challenge: add data at a specific index
+    temp = @head
+    new_link = temp.next
+
+    (index-1).times do
+      temp = temp.next
+      new_link = new_link.next
+    end
+
+    new_node = Node.new(data, nil)
+    if index == 0
+      @head = new_node
+      new_node.next = temp
+    else
+      temp.next = new_node
+      new_node.next = new_link
+    end
   end
 
   def delete_at_index(index)
     # TODO Challenge: remove data from a specific index
+    temp = @head
+    new_link = temp.next
+
+    (index-1).times do
+      temp = temp.next
+      new_link = new_link.next
+    end
+
+    if index == 0
+      @head = @head.next
+    else
+      new_link = new_link.next
+      temp.next = new_link
+    end
   end
 
   def contains?(data)
     # TODO Challenge: returns if the list contains data
+    temp = @head
+    while temp != nil
+      return true if temp.data == data
+      temp = temp.next
+    end
+
+    return false
   end
 
   def to_s
--- linked-list/test_list.rb
+++ linked-list/test_list.rb
@@ -5,5 +5,11 @@ x = List.new
 puts "Is the list empty? " + x.empty?.to_s
 puts x.to_s
 x.add("a")
-puts x.to_s
+x.add("b")
+x.add("c")
+x.add_at_index(0,"z") #[z][a][b][c]
+x.delete_at_index(1) # [z][b][c]
+puts "Does the list contain 'c'? #{x.contains?("c")}" # true
+puts "Does the list contain 'd'? #{x.contains?("d")}" # false
 puts "Is the list empty? " + x.empty?.to_s
+puts x.to_s

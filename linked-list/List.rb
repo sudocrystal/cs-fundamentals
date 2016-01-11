eae1f5b56608c0b84e28794c23c4975ccf867c99
Logan McDonald <loganmcdona11@gmail.com>
2016-01-11 12:15:30 -0800

    attempt at the linked list hw

--- linked-list/List.rb
+++ linked-list/List.rb
@@ -1,6 +1,9 @@
 require './Node.rb'
+require 'pry'
 
 class List
+  attr_accessor :head
+
   def initialize
 	  @head = nil
   end
@@ -10,24 +13,66 @@ class List
   end
 
   def add(data)
-    # if the list is empty, add the first node
 	  if @head == nil
 	    @head = Node.new(data,nil)
-	  end
-    # if the list isn't empty, add after the last node
-    # TODO: this case
+    end
+
+    temp = @head
+    while temp.next != nil 
+      temp = temp.next 
+    end
+    # binding.pry
+    temp.next = Node.new(data,nil)
+
+    # @head = Node.new(data,@head)
+    # THIS IS EXAMPLE ONLY
+    # b,a
+    # make b node
+    # b = @head
+    # reassign the head
   end
 
   def add_at_index(index, data)
-    # TODO Challenge: add data at a specific index
+    temp = @head
+    if index == 0 
+      @head = Node.new(data, temp)
+    else
+      (index - 1).times do 
+        temp = temp.next 
+      end
+      new_node = Node.new(data, temp.next)
+      temp.next = new_node
+    end
+  end
+
   end
 
   def delete_at_index(index)
-    # TODO Challenge: remove data from a specific index
+    temp = @head
+    if index == 0
+      if temp.next == nil 
+        @head = nil
+      else
+        @head = temp.next
+      end
+    else
+      (index - 1).times do
+        temp = temp.next
+      end
+
+      temp.next = temp.next.next
+    end
   end
 
   def contains?(data)
-    # TODO Challenge: returns if the list contains data
+    temp = @head 
+    while temp!= nil 
+      if temp.data == data
+        return true 
+      end
+      temp = temp.next 
+      return false 
+    end
   end
 
   def to_s
--- linked-list/test_list.rb
+++ linked-list/test_list.rb
@@ -5,5 +5,8 @@ x = List.new
 puts "Is the list empty? " + x.empty?.to_s
 puts x.to_s
 x.add("a")
+x.add("b")
 puts x.to_s
 puts "Is the list empty? " + x.empty?.to_s
+
+puts x.contains?("a")

c60fb838b99a7388f5b2099c180efaedebac174f
Ricky <houglande@gmail.com>
2016-01-11 21:58:02 -0800

    Create linked-list-practice.rb

---
+++ linked-list/linked-list-practice.rb
@@ -0,0 +1,32 @@
+require './List.rb'
+
+def count_node(list)
+  return "count_node not yet implemented"
+end
+
+def find_min(list)
+  # temp = @head
+  # min = nil
+  return "find_min not yet implemented"
+end
+
+def reverse(list)
+  # reversing a list. much harder (head becomes tail)
+  return "reverse not yet implemented"
+end
+
+# create a new linked list
+my_list = List.new
+
+# put some stuff in the list
+my_list.add(2)
+my_list.add(0)
+my_list.add(6)
+
+# test methods above
+puts "What's in my list?"
+puts my_list
+
+puts "NODE COUNT = #{count_node(my_list)}"
+puts "SMALLEST ELEMENT = #{find_min(my_list)}"
+puts "REVERSED LIST = #{reverse(my_list)}"

a34ca3603c5fb394dca30c674476f12869c6cd47
Tammy <misstamar@gmail.com>
2016-01-13 07:52:17 -0800

    completed contaie method

--- linked-list/List.rb
+++ linked-list/List.rb
@@ -1,6 +1,7 @@
 require './Node.rb'
 
 class List
+  attr_accessor :head
   def initialize
 	  @head = nil
   end
@@ -9,17 +10,31 @@ class List
 	  @head.nil?
   end
 
+
   def add(data)
     # if the list is empty, add the first node
 	  if @head == nil
 	    @head = Node.new(data,nil)
-	  end
-    # if the list isn't empty, add after the last node
-    # TODO: this case
+	  else
+      temp = @head
+      while temp.next!= nil
+        temp = temp.next
+      end
+      temp.next = Node.new(data,nil)
+    end
   end
 
   def add_at_index(index, data)
-    # TODO Challenge: add data at a specific index
+    if @head == nil
+	    @head = Node.new(data,nil)
+	  else
+      temp = @head
+      i = 1
+      while temp.next!= index || temp.next != nil
+        temp = temp.next
+      end
+      temp.next = Node.new(data,nil)
+    end
   end
 
   def delete_at_index(index)
@@ -27,7 +42,12 @@ class List
   end
 
   def contains?(data)
-    # TODO Challenge: returns if the list contains data
+    temp = @head
+    while temp!= nil
+      return true if temp.data == data
+      temp = temp.next
+    end
+    return false
   end
 
   def to_s
--- linked-list/test_list.rb
+++ linked-list/test_list.rb
@@ -5,5 +5,10 @@ x = List.new
 puts "Is the list empty? " + x.empty?.to_s
 puts x.to_s
 x.add("a")
+x.add("b")
+x.add("x")
 puts x.to_s
 puts "Is the list empty? " + x.empty?.to_s
+
+# How do I test it ?
+# puts x.add_at_index("b", "2")

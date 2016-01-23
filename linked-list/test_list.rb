6c8f0db7282c962e81238bebd464f7dc7b270555
Desiree <desireepoland@gmail.com>
2016-01-22 21:02:21 -0800

    make tests

--- linked-list/test_list.rb
+++ linked-list/test_list.rb
@@ -1,13 +1,30 @@
 require './List.rb'
 require './Node.rb'
+require './linked-list-practice.rb'
+require "minitest/autorun"
 
-x = List.new
-puts "Is the list empty? " + x.empty?.to_s
-puts x.to_s
-x.add("a")
-x.add("b")
-x.add("c")
-x.add_at_index(0,"z") # [z][a][b][c]
-#x.delete(1) # [z][b][c]
-puts "Is the list empty? " + x.empty?.to_s
-puts x.to_s
+class TestList < Minitest::Test
+  def setup
+    @list = List.new
+  end
+
+  def test_is_empty
+    assert @list.empty?
+  end
+
+  def test_adding
+    @list.add("a")
+    @list.add("b")
+    @list.add("c")
+    @list.add_at_index(0,"z") # [z][a][b][c]
+    assert_equal "List contents: [z][a][b][c]", @list.to_s
+    assert !@list.empty?
+  end
+
+  def test_reversal
+    @list.add("a")
+    @list.add("b")
+    @list.add("c")
+    assert_equal "List contents: [c][b][a]", reverse(@list).to_s
+  end
+end
